9efa6ac7cd19ba523888150e53e524c2e97fc6c1
duckburger <kmuravskiy@gmail.com>
2017-08-25 21:08:22 -0400

    Initial commit

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    
+    private paddle paddle;
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddle = GameObject.FindObjectOfType<paddle>();
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        
+        
+	}
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            transform.position = paddle.transform.position + paddleToBallVector;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Mouse clicked!");
+                hasStarted = true;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+        print(tweak);
+
+        if (hasStarted)
+        { 
+        GetComponent<AudioSource>().Play();
+        GetComponent<Rigidbody2D>().velocity += tweak;
+            
+        }
+    }
+
+}
---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+    
+    private int timesHit;
+    private LevelManager levelManager;
+    private bool isBreakable;
+
+    
+    public GameObject smoke;
+    public static int breakableCount = 0;
+    public AudioClip crack;
+    public Sprite[] hitSprites;
+
+	// Use this for initialization
+	void Start () {
+        
+        isBreakable = (this.tag == "Breakable");
+        // Keep track of breakable bricks
+        if (isBreakable)
+        {
+            breakableCount++;
+            
+        }
+
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+        timesHit = 0;
+	}
+
+    
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        AudioSource.PlayClipAtPoint(crack, transform.position);
+        if (isBreakable)
+        {
+            
+            HandleHits();
+            
+        }
+        
+    }
+
+    void HandleHits ()
+    {
+        int maxHits = hitSprites.Length + 1;
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            breakableCount--;
+            levelManager.BrickDestroyed();
+            Destroy(gameObject);
+            PuffSmoke();
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    void PuffSmoke ()
+    {
+        GameObject smokePuff = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+        smokePuff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+    void LoadSprites ()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        } else
+        {
+            Debug.LogError("Briick sprite is missing");
+        }
+    }
+
+    //TODO Remove this methind once we cab actually win!
+    void SimulateWin ()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
+
